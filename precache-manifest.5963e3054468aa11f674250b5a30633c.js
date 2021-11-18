58e4779006d98b03b15da68b65e75c7707df35f9
Innocent Thomas <innthomas00158@gmail.com>
2021-11-18 11:42:40 +0100

    Updates

---
+++ precache-manifest.5963e3054468aa11f674250b5a30633c.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d7835d9547190812ecdd4c3c81955b",
+    "url": "/strapi-x/index.html"
+  },
+  {
+    "revision": "a5fa9d4d8d0e75b5a961",
+    "url": "/strapi-x/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "6c27feb9ffa21efb0e6c",
+    "url": "/strapi-x/static/css/main.e30372a1.chunk.css"
+  },
+  {
+    "revision": "a5fa9d4d8d0e75b5a961",
+    "url": "/strapi-x/static/js/2.0ea776b1.chunk.js"
+  },
+  {
+    "revision": "435881bb28fec4d4e84baf9dfd194218",
+    "url": "/strapi-x/static/js/2.0ea776b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc6c36c7a0104d36719",
+    "url": "/strapi-x/static/js/3.9d04d768.chunk.js"
+  },
+  {
+    "revision": "6c27feb9ffa21efb0e6c",
+    "url": "/strapi-x/static/js/main.4fb4c072.chunk.js"
+  },
+  {
+    "revision": "a05726f31981abc98334",
+    "url": "/strapi-x/static/js/runtime-main.00351fa4.js"
+  },
+  {
+    "revision": "bfb4cca6418dbc93f0a0d38b773edf17",
+    "url": "/strapi-x/static/media/character-1.bfb4cca6.png"
+  },
+  {
+    "revision": "f82e820b3e930831bcec822e49631f9a",
+    "url": "/strapi-x/static/media/character-10.f82e820b.png"
+  },
+  {
+    "revision": "debe0e40a8ff4f9943c5e2b013148751",
+    "url": "/strapi-x/static/media/character-11.debe0e40.png"
+  },
+  {
+    "revision": "1c56105431048d7c555a84b512daaf5f",
+    "url": "/strapi-x/static/media/character-12.1c561054.png"
+  },
+  {
+    "revision": "5cfdc291eaeaa4ce732b1cccb1ccfbdc",
+    "url": "/strapi-x/static/media/character-13.5cfdc291.png"
+  },
+  {
+    "revision": "52b6d34ac1f340fde11f3e818f984b3c",
+    "url": "/strapi-x/static/media/character-14.52b6d34a.png"
+  },
+  {
+    "revision": "e109c59ba9f6fd7ae8346fe009a3bea3",
+    "url": "/strapi-x/static/media/character-15.e109c59b.png"
+  },
+  {
+    "revision": "65191d1b5d746bba15ec8698680635e0",
+    "url": "/strapi-x/static/media/character-16.65191d1b.png"
+  },
+  {
+    "revision": "2db106cc9211faa625054bb2d2aa6f32",
+    "url": "/strapi-x/static/media/character-2.2db106cc.png"
+  },
+  {
+    "revision": "47be8ce468cd12a406914efd8ab310a8",
+    "url": "/strapi-x/static/media/character-3.47be8ce4.png"
+  },
+  {
+    "revision": "bca0b0f7b0f963ee1ad5059249f035ca",
+    "url": "/strapi-x/static/media/character-4.bca0b0f7.png"
+  },
+  {
+    "revision": "10bc62d72ea9bb101719d88fdd5738a3",
+    "url": "/strapi-x/static/media/character-5.10bc62d7.png"
+  },
+  {
+    "revision": "48099b7c59f767cf3f2666a5fd2dc204",
+    "url": "/strapi-x/static/media/character-6.48099b7c.png"
+  },
+  {
+    "revision": "a84e5d38632cf3ac7fbc38f4d104e9da",
+    "url": "/strapi-x/static/media/character-7.a84e5d38.png"
+  },
+  {
+    "revision": "8849448f410f65973ac1b588897255ba",
+    "url": "/strapi-x/static/media/character-8.8849448f.png"
+  },
+  {
+    "revision": "6a191415eeaf0a80a1f750fc51aa2e95",
+    "url": "/strapi-x/static/media/character-9.6a191415.png"
+  },
+  {
+    "revision": "c3295ffbf3d061b75b0f85561fbde32d",
+    "url": "/strapi-x/static/media/planet-1.c3295ffb.svg"
+  },
+  {
+    "revision": "20ff06637d08046962b803d9ba19d766",
+    "url": "/strapi-x/static/media/planet-2.20ff0663.svg"
+  },
+  {
+    "revision": "1ce31053d7931f2a8f42070807997fce",
+    "url": "/strapi-x/static/media/planet-3.1ce31053.svg"
+  },
+  {
+    "revision": "ad13a4523e828722dce82de90dc37e7f",
+    "url": "/strapi-x/static/media/planet-4.ad13a452.svg"
+  },
+  {
+    "revision": "b038049b1f66fb55352ce08563374458",
+    "url": "/strapi-x/static/media/planet-5.b038049b.svg"
+  },
+  {
+    "revision": "7e1ed4cb99a51fbfe77ae7d78ce23cbe",
+    "url": "/strapi-x/static/media/planet-6.7e1ed4cb.svg"
+  },
+  {
+    "revision": "cbcdeffb0db68be24f9143849965d54b",
+    "url": "/strapi-x/static/media/planet-7.cbcdeffb.svg"
+  },
+  {
+    "revision": "2924508d556cd02e2a737419d3be811e",
+    "url": "/strapi-x/static/media/planet-8.2924508d.svg"
+  },
+  {
+    "revision": "9401a7061576a3a034992d3edfff5255",
+    "url": "/strapi-x/static/media/planet-9.9401a706.svg"
+  },
+  {
+    "revision": "2412ea79c7adb7b6b7fb283bdab7e0d5",
+    "url": "/strapi-x/static/media/planet-loader.2412ea79.svg"
+  }
+]);
